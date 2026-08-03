dc4141c5c3807e1fdb24f09c652d92d332c7c6c5
Yang-Tech-Lab <yanglao290@gmail.com>
2026-08-03 19:19:58 +0800

    feat: complete debug of forever.go

---
+++ forever/forever.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+     i:=1
+     for i<20 {
+         i+=i
+     }
+     fmt.Println(i)
+}
+
+
+//package main
+
+//func main(){
+//     for{
+
+//       }    
+//}
+
+//result:timeout running program
+//# is 'ILLEGAL'
