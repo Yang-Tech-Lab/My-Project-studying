570ce5bfd3ee013ae4a6bf9a3ba6de6bcb8ac8e0
Yang-Tech-Lab <yanglao290@gmail.com>
2026-08-14 16:54:39 +0800

    refactor: flatten folder structure

--- switch-evaluation-order/switch-evaluation-order.go
+++ switch-evaluation-order/switch-evaluation-order.go
@@ -10,12 +10,13 @@ func main() {
 	today := time.Now().Weekday()
 	switch time.Saturday {
 	case today + 0:
-		fmt.Println("Today")
+		fmt.Println("Today.")
 	case today + 1:
-		fmt.Println("Tomorrow")
+		fmt.Println("Tomorrow.")
 	case today + 2:
-		fmt.Println("In two days")
+		fmt.Println("In two days.")
 	default:
-		fmt.Println("Too far away")
+		fmt.Println("Too far away.")
 	}
+
 }
